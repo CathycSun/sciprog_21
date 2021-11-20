5a3199dfb7e14b02bcc05595703ee3c283ad8771
CathycSun <ruijia.sun@ucdconnect.ie>
2021-11-20 21:35:24 +0000

    added to practical03

---
+++ practical03/Printing.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void){
+
+   //Declare two integers variables
+   //and assign values
+   int i1=10, i2=200;
+
+   //Decleare two floats variables, single precision
+   //and assign values
+   float fl1=1.11, fl2=2.2222222;
+
+   //Print the values of the two integers, format %d, and go to new line, format \n
+   //and the two floats, format %.nf (floats with n decimal places)
+   printf("Two integers are %d , %d and two floats are %.5f ,  %.5f\n", i1, i2, fl1, fl2);
+   printf("Two integers are %d , %d and two floats are %.7f ,  %.7f\n", i1, i2, fl1, fl2);
+
+return 0;
+}
---
+++ practical03/README.txt
@@ -0,0 +1,6 @@
+There are the files for the third week practical.
+
+File "Printing.c" aims at printing by C.
+File "Write.f90" aims at printing by Fortran.
+
+Both of the files "Trapezodial.c" and "Trapezodial.f90" calculate the area under the curve of f(x) using Trapezodial rule.
